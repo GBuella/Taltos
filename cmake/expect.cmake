5be6c0d1137f9ea3e3c9c6fc2df3a16aeeb85019
Gabor Buella <gbuella@gmail.com>
2016-10-01 00:53:58 +0200

    Refactoring test - use "cmake -P compare_files" instead of custom script

---
+++ cmake/expect.cmake
@@ -0,0 +1,19 @@
+# Run a program with input piped into, and compare the output
+# with the expected output.
+
+execute_process(COMMAND ${TEST_PROG}
+	INPUT_FILE ${TEST_FILE}.in
+	OUTPUT_FILE .out
+	ERROR_FILE .err
+	RESULT_VARIABLE HAD_ERROR)
+
+if(HAD_ERROR)
+	message(FATAL_ERROR "Test failed")
+endif()
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E compare_files .out ${TEST_FILE}.out
+	RESULT_VARIABLE DIFFERENT)
+
+if(DIFFERENT)
+	message(FATAL_ERROR "Test failed - files differ")
+endif()
--- util/runtest.sh
+++
@@ -1,9 +0,0 @@
-prog=$1
-test=$2
-$prog --notest < $test.in > .out
-if [ $? -ne 0 ]
-then
-	exit 1
-exit
-fi
-cmp .out $test.out
