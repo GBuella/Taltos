bd9d67943fdc0be72cb473183fc07bd3c4937fe9
Gabor Buella <gbuella@gmail.com>
2017-01-06 23:31:51 +0100

    main.c fix display name

--- src/main.c
+++ src/main.c
@@ -122,7 +122,7 @@ setup_defaults(void)
 static void
 setup_display_name(void)
 {
-	static char name[0x100] = "t3 Taltos";
+	static char name[0x100] = "Taltos";
 
 	if (!conf.search.use_LMR)
 		strcat(name, "-noLMR");
