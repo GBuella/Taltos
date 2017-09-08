2785451e73215014a0a7d0cb8eb2904383096f2e
Gabor Buella <gbuella@gmail.com>
2017-09-09 00:45:52 +0200

    Improve fen_book test
    Use file from repo, instead of creating it on the fly.

--- tests/books/fen_book.c
+++ tests/books/fen_book.c
@@ -7,52 +7,42 @@
 #include "book.h"
 #include "position.h"
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 void
 run_tests(void)
 {
+	if (prog_argc < 2)
+		exit(EXIT_FAILURE);
+
 	struct {
-		const char *line;
+		const char *fen;
 		move moves[4];
 	} test_book[] = {
 
-	{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - e4 Nf3 d4",
+	{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RN1QKBNR w KQkq -",
 		{ create_move_pd(sq_e2, sq_e4),
 		    create_move_g(sq_g1, sq_f3, knight, 0, false),
 		    create_move_pd(sq_d2, sq_d4),
 	            0, }},
-	{ "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - e5 c5",
+	{ "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq -",
 		{ create_move_pd(sq_e2, sq_e4),
 		    create_move_pd(sq_c2, sq_c4),
 	            0, }},
 	};
 
-	static const char path[] = "test_fen_book.txt";
-	FILE *f = fopen(path, "w");
-	if (f == NULL) {
-		perror(path);
-		exit(EXIT_FAILURE);
-	}
+	assert(book_open(bt_fen, "/invalid_path") == NULL);
 
-	for (size_t i = 0; i < ARRAY_LENGTH(test_book); ++i) {
-		if (fputs(test_book[i].line, f) <= 0) {
-			perror(path);
-			exit(EXIT_FAILURE);
-		}
-		if (fputc('\n', f) == EOF) {
-			perror(path);
-			exit(EXIT_FAILURE);
-		}
-	}
-	fclose(f);
-
-	struct book *book = book_open(bt_fen, path);
+	errno = 0;
+	struct book *book = book_open(bt_fen, prog_argv[1]);
 	if (book == NULL) {
-		perror(path);
+		perror(prog_argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
+	assert(book_get_size(book) == 3);
 
 	for (size_t i = 0; i < ARRAY_LENGTH(test_book); ++i) {
 		struct position pos;
@@ -60,7 +50,7 @@ run_tests(void)
 		int ep_index;
 		move moves[MOVE_ARRAY_LENGTH];
 
-		position_read_fen(&pos, test_book[i].line, &ep_index, &turn);
+		position_read_fen(&pos, test_book[i].fen, &ep_index, &turn);
 
 		book_get_move_list(book, &pos, moves);
 
